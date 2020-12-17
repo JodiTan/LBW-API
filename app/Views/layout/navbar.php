b0a0a0b953471b86a4288116368bbbfd46b34290
William Oktavianus <williamoktavianus1099@gmail.com>
2020-12-17 10:01:41 +0700

    Penambahan search navbar
    Menambahkan search di bagian navbar.

--- app/Views/layout/navbar.php
+++ app/Views/layout/navbar.php
@@ -4,6 +4,7 @@
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
+
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item">
@@ -17,5 +18,10 @@
                 </li>
             </ul>
         </div>
+
+        <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
     </div>
 </nav>
