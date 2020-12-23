c7eb50b6971b32f49c6524726fb48b4029954623
William Oktavianus <williamoktavianus1099@gmail.com>
2020-12-23 14:12:43 +0700

    Perbaikan page navigation (peoples)
    Mempercantik tampilan page navigation pada halaman peoples.

--- app/Views/pages/peoples.php
+++ app/Views/pages/peoples.php
@@ -43,17 +43,31 @@
 
 		<nav aria-label="Page navigation example">
 			<ul class="pagination">
-				<li class="page-item <?php echo $page == 1 ? "disabled" : ""; ?>"><a class="page-link" href="<?= base_url('/peoples/') ?>">First</a></li>
+				<li class="page-item <?php echo $page == 1 ? "disabled" : ""; ?>"><a class="page-link" href="<?= base_url('/peoples/') ?>"><i class="fa fa-angle-double-left"></i></a></li>
+				<?php
+				if ($page > 1) {
+				?>
 				<li class="page-item"><a class="page-link" href="<?= $page - 1 >= 1 ? base_url('/peoples/' . ($page - 1)) : ""; ?>">
 					<?php echo $page - 1 >= 1 ? $page - 1 : ""; ?>
 				</a></li>
+				<?php
+				}
+				?>
+
 				<li class="page-item active"><a class="page-link">
 					<?php echo $page ?>
 				</a></li>
+
+				<?php
+				if ($page < $lastPage) {
+				?>
 				<li class="page-item"><a class="page-link" href="<?= $page + 1 <= $lastPage ? base_url('/peoples/' . ($page + 1)) : ""; ?>">
 					<?php echo $page + 1 <= $lastPage ? $page + 1 : ""; ?>
 				</a></li>
-				<li class="page-item <?php echo $page == $lastPage ? "disabled" : ""; ?>"><a class="page-link" href="<?= base_url('/peoples/' . $lastPage) ?>">Last</a></li>
+				<?php
+				}
+				?>
+				<li class="page-item <?php echo $page == $lastPage ? "disabled" : ""; ?>"><a class="page-link" href="<?= base_url('/peoples/' . $lastPage) ?>"><i class="fa fa-angle-double-right"></i></a></li>
 			</ul>
 		</nav>
 	</div>
