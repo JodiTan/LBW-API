4a32e84c0bf1c5a797bc636b091834e58a7774b4
JodiTan <jodi.tanato@gmail.com>
2020-12-30 16:56:11 +0700

    [UPDATE] carousel

--- app/Views/pages/movies.php
+++ app/Views/pages/movies.php
@@ -1,82 +1,77 @@
 <?= $this->extend('layout/template'); ?>
 
 <?= $this->section('content'); ?>
-<div class="container mt-3">
-	<h1> Movies Page</h1>
-</div>
 
 <div>
-	<div>
-		<h2 class="container mt-3">
-			Now Playing
-		</h2>
-	</div>
 		
-	<!-- <div id="carouselExampleControls" class="carousel slide container" data-ride="carousel">
-		<div class="carousel-inner">
-			<div class="carousel-item active bg-dark">
-				<div alt="First Slide" class="row justify-content-center">
-					<div class="mx-2">
-						<image src=<?php echo base_url('images/A_Beautiful_Mind.jpg') ?> style="width:150px; height:200px"/>
-					</div>
-					<div class="mx-2">
-						<image src=<?php echo base_url('images/Black_Panther.jpg') ?> style="width:150px; height:200px"/>
-					</div>
-					<div class="mx-2">
-						<image src=<?php echo base_url('images/Frozen_2.jpg') ?> style="width:150px; height:200px"/>
+
+	<!-- Movies list -->
+	<?php
+	$movies = $nowPlaying["results"];
+	if(count($movies) > 0) {
+		?>
+		<div class="container mb-2">
+			<h3>Now Playing</h3>
+			<div id="moviesCarousel" class="carousel slide row" data-interval="false">
+				<div class="col">
+					<a class="carousel-control-prev text-dark" href="#moviesCarousel" role="button" data-slide="prev">
+						<span class="fa fa-chevron-left" aria-hidden="true"></span>
+						<span class="sr-only">Previous</span>
+					</a>
+				</div>
+	
+				<div class="col-11 carousel-inner">
+					<div class="carousel-item active">
+						<div class="row mb-2">
+							<?php
+							$counter = 0;
+							foreach ($movies as $movie) {
+								if ($counter < 2) {
+									$counter++;
+								} else {
+									$counter = 1;
+									?>
+										</div>
+									</div>
+									<div class="carousel-item">
+										<div class="row mb-2">
+									<?php
+								}
+								?>
+								<div class="col-sm text-center">
+									<div class="card h-100">
+										<img class="card-img-top h-auto" src="<?php echo "https://image.tmdb.org/t/p/w500/" . $movie["poster_path"]; ?>" onerror="this.onerror=null;this.src='<?= base_url('images/not-available.png') ?>'" alt="<?php echo $movie["title"] . " Poster"; ?>">
+										<div class="card-body text-center">
+											<h3 class="card-title d-flex "> <a href=""> <?php echo $movie["title"]; ?> </a> </h3>
+										</div>
+									</div>
+								</div>
+								<?php
+							}
+	
+							if ($counter > 0) {
+								while($counter < 2) {
+									$counter++;
+									?>
+									<div class="col-sm">&nbsp;</div>
+									<?php
+								}
+							}
+							?>
+						</div>
 					</div>
 				</div>
-			</div>
-			<div class="carousel-item container">
-				<div alt="Second Slide" class="row justify-content-center bg-dark">
-					<div class="mx-2">
-						<image src=<?php echo base_url('images/Frozen_2.jpg') ?> style="width:150px; height:200px"/>
-					</div>
-					<div class="mx-2">
-						<image src=<?php echo base_url('images/A_Beautiful_Mind.jpg') ?> style="width:150px; height:200px"/>
-					</div>
-					<div class="mx-2">
-						<image src=<?php echo base_url('images/Black_Panther.jpg') ?> style="width:150px; height:200px"/>
-					</div>
+	
+				<div class="col">
+					<a class="carousel-control-next text-dark" href="#moviesCarousel" role="button" data-slide="next">
+						<span class="fa fa-chevron-right" aria-hidden="true"></span>
+						<span class="sr-only">Next</span>
+					</a>
 				</div>
 			</div>
-			<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-				<span class="sr-only">Previous</span>
-			</a>
-			<a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-				<span class="carousel-control-next-icon" aria-hidden="true"></span>
-				<span class="sr-only">Next</span>
-			</a>
 		</div>
-	</div> -->
-	<div class="row">
-		<!-- People list -->
 		<?php
-		$movies = $nowPlaying["results"];
-		$counter = 0;
-		foreach ($movies as $movie) {
-			if ($counter < 5) {
-				$counter++;
-			} else {
-				$counter = 1;
-				?>
-				</div>
-				<div class="row">
-				<?php
-			}
-			?>
-			<div class="col m-2">
-				<div class="card h-100">
-					<img class="card-img-top custom-card-image" src="<?php echo "https://image.tmdb.org/t/p/w500/" . $movie["poster_path"]; ?>" onerror="this.onerror=null;this.src='<?= base_url('images/not-available.png') ?>'" alt="<?php echo $movie["title"] . " Title"; ?>">
-					<div class="card-body">
-						<p class="card-title d-flex"> <a href="<?= base_url("/profile/" . $movie["id"]); ?>"> <?php echo $movie["title"]; ?> </a> </p>
-					</div>
-				</div>
-			</div>
-			<?php
 		}
 		?>
-	</div>
 </div>	
 <?= $this->endSection('content'); ?>
