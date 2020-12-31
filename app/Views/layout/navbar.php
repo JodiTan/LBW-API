9ce4b1bf2c87e7904e67a92ac70d1d483f27ed3e
William Oktavianus <williamoktavianus1099@gmail.com>
2021-01-01 00:56:44 +0700

    Penambahan logo
    Menambahkan logo (coba-coba).

--- app/Views/layout/navbar.php
+++ app/Views/layout/navbar.php
@@ -1,6 +1,8 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container">
-        <a class="navbar-brand" href="<?= base_url('/'); ?>">MovieLBW</a>
+        <a class="navbar-brand" href="<?= base_url('/'); ?>">
+            <img id="home-logo" src="<?= base_url('images/logo.png'); ?>" alt="MovieLBW">
+        </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
