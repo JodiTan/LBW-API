dc6c45430fcf075f995bb77825f2c916784e5482
William Oktavianus <williamoktavianus1099@gmail.com>
2020-12-31 18:45:17 +0700

    Penambahan footer
    Menambahkan footer sederhana.

---
+++ app/Views/layout/footer.php
@@ -0,0 +1,13 @@
+<!-- Site footer -->
+<footer class="site-footer">
+    <div class="container mt-4">
+        <hr>
+        <p class="text-center">
+            All data are provided by The Movie Database.
+        </p>
+
+        <p class="text-center text-secondary">
+            &#169; 2020 MovieLBW
+        </p>
+    </div>
+</footer>
