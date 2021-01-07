dc3055f2fd976672efe6a8b5fdafe687bf803dc4
Melody Victorian <50192955+melodyvictorian22@users.noreply.github.com>
2021-01-07 11:17:37 +0700

    perbaikan minor

--- app/Views/pages/recommendation.php
+++ app/Views/pages/recommendation.php
@@ -3,6 +3,7 @@
 <?= $this->section('content'); ?>
 <div class="container">
 	<div class="row-my-3">
+	<br>
 	<h1> Recommendation </h1>
 	</div>
 	<div class="row">
@@ -26,6 +27,7 @@
 					<img class="card-img-top custom-card-image" src="<?php echo "https://image.tmdb.org/t/p/w500/" . $movie["poster_path"]; ?>" onerror="this.onerror=null;this.src='<?= base_url('images/not-available.png') ?>'" alt="<?php echo $movie["title"] . " Title"; ?>">
 					<div class="card-body">
 						<p class="card-title d-flex"> <a href="<?= base_url("/profile/" . $movie["id"]); ?>"> <?php echo $movie["title"]; ?> </a> </p>
+						<p class="card-subtitle mb-2 text-muted"> <?php echo isset($movie["release_date"]) ? substr($movie["release_date"], 0, 4) : "Upcoming"; ?> </p>
 						<p class="card-text"> <i class="fa fa-fire" style="color:red;"></i> <?php echo $movie["popularity"]; ?> </p>
 					</div>
 				</div>
