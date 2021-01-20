c3fad39c1b9e3e15cdc387b48a6645865ad81d53
William Oktavianus <williamoktavianus1099@gmail.com>
2021-01-20 20:43:08 +0700

    Perapihan minor
    Merapikan kode program (minor) untuk Search.

--- app/Models/MultiSearchModel.php
+++ app/Models/MultiSearchModel.php
@@ -6,12 +6,12 @@ use CodeIgniter\Model;
 
 class MultiSearchModel extends Model
 {
-    protected $api_key = '2695db7da16dc8dc807f8deb23b67567';
+    protected $apiKey = '2695db7da16dc8dc807f8deb23b67567';
 
     public function getSearchResult($page = 1, $query) {
         $ch = curl_init();
         $escaped_query = rawurlencode($query);
-        curl_setopt($ch, CURLOPT_URL, "https://api.themoviedb.org/3/search/multi?api_key=$this->api_key&query=$escaped_query&page=$page");
+        curl_setopt($ch, CURLOPT_URL, "https://api.themoviedb.org/3/search/multi?api_key=$this->apiKey&query=$escaped_query&page=$page");
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $output = json_decode(curl_exec($ch), true);
         curl_close($ch);
